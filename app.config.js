b320d83c8208dce483ab8cabbbeb431a96ba3f6a
andilupu92 <andilupu92@gmail.com>
2026-06-26 18:07:22 +0300

    switch to app.config.js for expo secrets

---
+++ app.config.js
@@ -0,0 +1,49 @@
+export default {
+  expo: {
+    name: "karvio",
+    slug: "karvio",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/karvio.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/karvio-splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      bundleIdentifier: "com.anonymous.karvioapp",
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/karvio.png",
+        backgroundColor: "#202e5a"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.anonymous.karvioapp",
+      googleServicesFile: process.env.ANDROID_GOOGLE_SERVICES_FILE || "./google-services.json"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      "expo-font",
+      "@react-native-community/datetimepicker",
+      [
+        "@react-native-google-signin/google-signin",
+        {
+          iosUrlScheme: "com.googleusercontent.apps.31878404477-1lc32398bo1k30il8ec3sqdmjlkhhhg8"
+        }
+      ]
+    ],
+    extra: {
+      eas: {
+        projectId: "9d4129ec-0ba9-4e61-b0a7-78ee82561b0f"
+      }
+    },
+    owner: "andreilupu92"
+  }
+};
